5fab09f8e33f2d1418cbccfd1cccefcfac796a48
David Pathakjee <dpathakjee@gmail.com>
2015-10-15 07:31:29 +0000

    split out os x config

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -1,6 +1,7 @@
 ln -s ~/homedir/.zshrc ~/.zshrc
 ln -s ~/homedir/.gitconfig ~/.gitconfig
 ln -s ~/homedir/.tmux.conf ~/.tmux.conf
+ln -s ~/homedir/.tmux-osx.conf ~/.tmux-osx.conf
 
 mkdir -p ~/bin
 rm ~/bin/util;
